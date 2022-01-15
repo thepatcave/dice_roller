5018da5008491edadd647ab2590ca15fa5ed44f4
Patrick Quinn <91748394+thepatcave@users.noreply.github.com>
2022-01-14 21:06:56 -0600

    Added while loop and sum result

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,24 @@
 import random
 
-print("Enter your dice roll (ex. 2d6):")
-dice = input()
-print()
+dice = ''
+while dice not in ['quit','q']:
+    print("Enter your dice roll (ex. 2d6):")
+    dice = input()
+    print()
 
-val = dice.split("d")
-numDice = int(val[0])
-valDice = int(val[1])
+    if dice not in ['quit','q']:
+        val = dice.split("d")
+        numDice = int(val[0])
+        valDice = int(val[1])
 
-rolls = []
-for r in range(numDice):
-    roll = random.randint(1,valDice)
-    rolls.append(roll)
-    
-print("Dice Rolled:", dice)
-print("Result(s):", rolls)
-print()
+        rolls = []
+        for r in range(numDice):
+            roll = random.randint(1,valDice)
+            rolls.append(roll)
+            
+        print("Dice Rolled:", dice)
+        print("Result(s):", rolls)
+        if numDice > 1:
+            print("Sum:", sum(rolls))
+        print()
+exit
