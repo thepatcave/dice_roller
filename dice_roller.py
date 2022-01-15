cc65d8adfb4a74f60428635bf34088d5ff01cbac
Patrick Quinn <91748394+thepatcave@users.noreply.github.com>
2022-01-15 00:30:06 -0600

    Enclosed in main() function

--- dice_roller.py
+++ dice_roller.py
@@ -3,33 +3,35 @@
 import random
 import re
 
-dPool = ''
-while dPool not in ['quit','q']:
-    print('\n' + 'Enter your dice roll (ex. 2d6+1d4) or \'q\' to quit:')
-    dPool = input().lower()
-    valid = re.search(r'\d+d\d+', dPool)
+def main():
+    dPool = ''
+    while dPool not in ['quit','q']:
+        print('\n' + 'Enter your dice roll (ex. 2d6+1d4) or \'q\' to quit:')
+        dPool = input().lower()
+        valid = re.search(r'\d+d\d+', dPool)
 
-    if valid:
-        dice = re.findall(r'\d+d\d+', dPool)
+        if valid:
+            dice = re.findall(r'\d+d\d+', dPool)
 
-        print('\n' + 'Dice Rolled:' + ' {}'.format(dPool))
-        print('\n' + 'Result(s):')
+            print('\n' + 'Dice Rolled:' + ' {}'.format(dPool))
+            print('\n' + 'Result(s):')
 
-        tRolls = []
-        for die in dice:
-            val = die.split("d")
-            numDice = int(val[0])
-            valDice = int(val[1])
+            tRolls = []
+            for die in dice:
+                val = die.split("d")
+                numDice = int(val[0])
+                valDice = int(val[1])
 
-            rolls = []
-            for r in range(numDice):
-                roll = random.randint(1,valDice)
-                rolls.append(roll)
-            print("  {} = {}; Sum = {}".format(die, rolls, sum(rolls)))
-            tRolls.append(sum(rolls))
-            
-        print('\n' + 'Total:' + ' {}'.format(sum(tRolls)))
-        print()
+                rolls = []
+                for r in range(numDice):
+                    roll = random.randint(1,valDice)
+                    rolls.append(roll)
+                print("  {} = {}; Sum = {}".format(die, rolls, sum(rolls)))
+                tRolls.append(sum(rolls))
+                
+            print('\n' + 'Total:' + ' {}'.format(sum(tRolls)))
+            print()
 
+main()
 print()
 exit
