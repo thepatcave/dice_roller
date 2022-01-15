fa0df23c78d9fd6a745c8c20744a51ea4be00d1e
Patrick Quinn <91748394+thepatcave@users.noreply.github.com>
2022-01-15 00:26:10 -0600

    Updated output formatting

--- dice_roller.py
+++ dice_roller.py
@@ -3,28 +3,33 @@
 import random
 import re
 
-print()
-dpool = ''
-while dpool not in ['quit','q']:
-    print("Enter your dice roll (ex. 2d6+1d4) or 'q' to quit:")
-    dpool = input().lower()
-    print()
+dPool = ''
+while dPool not in ['quit','q']:
+    print('\n' + 'Enter your dice roll (ex. 2d6+1d4) or \'q\' to quit:')
+    dPool = input().lower()
+    valid = re.search(r'\d+d\d+', dPool)
 
-    if dpool not in ['quit','q']:
-        dice = re.findall(r'\d+d\d+', dpool)
+    if valid:
+        dice = re.findall(r'\d+d\d+', dPool)
 
-        rolls = []
+        print('\n' + 'Dice Rolled:' + ' {}'.format(dPool))
+        print('\n' + 'Result(s):')
+
+        tRolls = []
         for die in dice:
             val = die.split("d")
             numDice = int(val[0])
             valDice = int(val[1])
 
+            rolls = []
             for r in range(numDice):
                 roll = random.randint(1,valDice)
                 rolls.append(roll)
+            print("  {} = {}; Sum = {}".format(die, rolls, sum(rolls)))
+            tRolls.append(sum(rolls))
             
-        print("Dice Rolled:", dpool)
-        print("Result(s):", rolls)
-        print("Sum:", sum(rolls))
+        print('\n' + 'Total:' + ' {}'.format(sum(tRolls)))
         print()
+
+print()
 exit
